bda64e0b737a958e84dd0119d7c2b5cf5fe14bd5
Williams Hernández <hernandw@gmail.com>
2024-08-07 15:28:37 -0400

    :sparkles: logica del carrito de compras

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,38 @@
-import { useState } from "react"
-import Header from "./components/Header"
-import {db} from "./data/data.js"
-import Guitar from "./components/Guitar"
-
+import { useState } from "react";
+import Header from "./components/Header";
+import { db } from "./data/data.js";
+import Guitar from "./components/Guitar";
 
 const App = () => {
-  const [guitar, setGuitar] = useState(db)
+  const [guitar, setGuitar] = useState(db);
+  const [total, setTotal] = useState(0);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => {
+    const itemExist = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExist  >= 0 ) {
+     const updateCart = [...cart];
+     updateCart[itemExist].quantity++;
+     setCart(updateCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  };
   return (
     <>
-    <Header />
-    <main className="container-xl mt-5">
+      <Header cart={cart} />
+      <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            {
-              guitar.map((guitar) => (
-                <Guitar key={guitar.id} guitar={guitar} />
-              ))
-            }
-
-            
-
-         
-
-          
+          {guitar.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} total={total}  />
+          ))}
         </div>
-    </main>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Guitar.tsx
+++ src/components/Guitar.tsx
@@ -1,6 +1,9 @@
 
 
-const Guitar = ({ guitar }) => {
+const Guitar = ({ guitar, addToCart }) => {
+  const handleCart = () => {
+    setCart([...cart, guitar]);
+  }
   const { id, name, description, image, price } = guitar;
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -17,7 +20,7 @@ const Guitar = ({ guitar }) => {
           {description}
         </p>
         <p className="fw-black text-primary fs-3">${price}</p>
-        <button type="button" className="btn btn-dark w-100">
+        <button type="button" className="btn btn-dark w-100" onClick={()=>addToCart(guitar)}>
           Agregar al Carrito
         </button>
       </div>
